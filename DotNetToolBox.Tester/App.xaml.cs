e64adb2c1feb255813b011553e30f8dc517aa4a7
Josué Clément <mod6991@gmail.com>
2020-02-27 17:34:35 +0100

    Add new tester project

---
+++ DotNetToolBox.Tester/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DotNetToolBox.Tester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+            ViewModel.MainWindowViewModel vm = new ViewModel.MainWindowViewModel(window);
+            window.DataContext = vm;
+            window.Show();
+        }
+    }
+}
