24244b121458ec3ab22db0d3fdac790677d39f78
Josué Clément <mod6991@gmail.com>
2020-03-01 22:26:56 +0100

    tests

--- DotNetToolBox.Tester/App.xaml.cs
+++ DotNetToolBox.Tester/App.xaml.cs
@@ -4,6 +4,7 @@ using DotNetToolBox.RibbonDock;
 using DotNetToolBox.RibbonDock.Ribbon;
 using DotNetToolBox.Tester.View;
 using DotNetToolBox.Tester.ViewModel;
+using System;
 using System.Linq;
 using System.Windows;
 
@@ -20,39 +21,44 @@ namespace DotNetToolBox.Tester
         {
             base.OnStartup(e);
 
-            RibbonDock.RibbonDockWindow win = RibbonDockWindowViewModel.GetNewWindow();
-            vm = (RibbonDockWindowViewModel)win.DataContext;
+            try
+            {
+                RibbonDock.RibbonDockWindow win = RibbonDockWindowViewModel.GetNewWindow();
+                vm = (RibbonDockWindowViewModel)win.DataContext;
 
-            RibbonButtonViewModel button = new RibbonButtonViewModel("test button");
-            button.Command = new RelayCommand(ButtonClick, ReturnTrue);
-            button.LargeImage = PngIcons.GetIcon(IconName.Home, IconSize.Size32);
-            vm.AddRibbonButton("HomeTab", "1st group", button);
+                RibbonButtonViewModel button = new RibbonButtonViewModel("test button");
+                button.Command = new RelayCommand(ButtonClick, ReturnTrue);
+                button.LargeImage = PngIcons.GetIcon(IconName.Home, IconSize.Size32);
+                vm.AddRibbonButton("HomeTab", "1st group", button);
 
-            RibbonButtonViewModel button2 = new RibbonButtonViewModel("test button 2");
-            button2.Command = new RelayCommand(ButtonClick2, ReturnTrue);
-            button2.LargeImage = PngIcons.GetIcon(IconName.Help, IconSize.Size32);
-            vm.AddRibbonButton("HomeTab", "1st group", button2);
+                RibbonButtonViewModel button2 = new RibbonButtonViewModel("test button 2");
+                button2.Command = new RelayCommand(ButtonClick2, ReturnTrue);
+                button2.LargeImage = PngIcons.GetIcon(IconName.Help, IconSize.Size32);
+                vm.AddRibbonButton("HomeTab", "1st group", button2);
 
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(ConfigurationViewModel), typeof(ConfigurationView));
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(DatabaseViewModel), typeof(DatabaseView));
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(GlobalViewModel), typeof(GlobalView));
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(HttpServerViewModel), typeof(HttpServerView));
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(ReportingViewModel), typeof(ReportingView));
-            DockDataTemplateSelector.RegisterDockViewModel(typeof(TcpServerViewModel), typeof(TcpServerView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(ConfigurationViewModel), typeof(ConfigurationView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(DatabaseViewModel), typeof(DatabaseView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(GlobalViewModel), typeof(GlobalView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(HttpServerViewModel), typeof(HttpServerView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(ReportingViewModel), typeof(ReportingView));
+                DockDataTemplateSelector.RegisterDockViewModel(typeof(TcpServerViewModel), typeof(TcpServerView));
 
-            vm.Documents.Add(new GlobalViewModel(vm));
-            vm.Documents.Add(new DatabaseViewModel(vm));
-            vm.Documents.Add(new ConfigurationViewModel(vm));
-            vm.Documents.Add(new HttpServerViewModel(vm));
-            vm.Documents.Add(new TcpServerViewModel(vm));
-            vm.Documents.Add(new ReportingViewModel(vm));
+                vm.Documents.Add(new GlobalViewModel(vm));
+                vm.Documents.Add(new DatabaseViewModel(vm));
+                vm.Documents.Add(new ConfigurationViewModel(vm));
+                vm.Documents.Add(new HttpServerViewModel(vm));
+                vm.Documents.Add(new TcpServerViewModel(vm));
+                vm.Documents.Add(new ReportingViewModel(vm));
 
-            vm.ActiveDocument = vm.Documents.First(x => x.ContentId == "Global");
+                vm.ActiveDocument = vm.Documents.First(x => x.ContentId == "Global");
 
-            //DockDataTemplateSelector.RegisterDockViewModel(typeof(TestDocument), typeof(TestDocumentView));
-            //vm.Documents.Add(new TestDocument(vm));
-
-            win.Show();
+                win.Show();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
         }
 
         private bool ReturnTrue(object param)
--- DotNetToolBox.Tester/ViewModel/HttpServerViewModel.cs
+++ DotNetToolBox.Tester/ViewModel/HttpServerViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using DotNetToolBox.Icons.OpenIconLibrary;
 using DotNetToolBox.MVVM;
+using DotNetToolBox.Net;
 using DotNetToolBox.RibbonDock;
 using DotNetToolBox.RibbonDock.Dock;
 using System;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
 using System.Windows.Input;
 
 namespace DotNetToolBox.Tester.ViewModel
@@ -12,6 +18,11 @@ namespace DotNetToolBox.Tester.ViewModel
         private RibbonDockWindowViewModel _rdVM;
         private ICommand _closeCommand;
         private ICommand _closeAllButThisCommand;
+        private ICommand _startCommand;
+        private ICommand _stopCommand;
+        private int _port = 8885;
+        private HttpServer _server;
+        private ObservableCollection<HttpListenerContext> _eventList;
 
         #region Constructor
 
@@ -20,12 +31,34 @@ namespace DotNetToolBox.Tester.ViewModel
             _rdVM = rdVM;
             _closeCommand = new RelayCommand(Close, ReturnTrue);
             _closeAllButThisCommand = new RelayCommand(CloseAllButThis, ReturnTrue);
+            _startCommand = new RelayCommand((param) => Start(), ReturnTrue);
+            _stopCommand = new RelayCommand((param) => Stop(), ReturnTrue);
             CanClose = false;
             CanFloat = true;
             ContentId = "HttpServer";
             Title = "HttpServer ";
             IsActive = true;
             IconSource = PngIcons.GetIcon(IconName.Settings, IconSize.Size16);
+            _eventList = new ObservableCollection<HttpListenerContext>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Port
+        {
+            get { return _port; }
+            set
+            {
+                _port = value;
+                OnPropertyChanged("Port");
+            }
+        }
+
+        public ObservableCollection<HttpListenerContext> EventList
+        {
+            get { return _eventList; }
         }
 
         #endregion
@@ -40,6 +73,16 @@ namespace DotNetToolBox.Tester.ViewModel
         public override ICommand CloseAllButThisCommand
         {
             get { return _closeAllButThisCommand; }
+        }
+
+        public ICommand StartCommand
+        {
+            get { return _startCommand; }
+        }
+
+        public ICommand StopCommand
+        {
+            get { return _stopCommand; }
         }
 
         #endregion
@@ -58,6 +101,26 @@ namespace DotNetToolBox.Tester.ViewModel
 
         private void CloseAllButThis(object param)
         {
+
+        }
+
+        private void Start()
+        {
+            _server = new HttpServer(new string[] { "http://*:8885/" });
+            _server.RequestHandler = HandleRequest;
+            _server.Start();
+        }
+
+        private void Stop()
+        {
+            _server.Close();
+        }
+
+        private void HandleRequest(HttpListenerContext context)
+        {
+            _eventList.Add(context);
+            byte[] data = Encoding.ASCII.GetBytes("Hello world !");
+            context.Response.OutputStream.Write(data, 0, data.Length);
 
         }
 
