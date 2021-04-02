a7eb91a40d90e3d96288f6222a2d347efcd75c0d
Josue Clement <mod6991@gmail.com>
2021-04-02 15:05:31 +0200

    Add SHA3 class

---
+++ DotNetToolBox/Cryptography/SHA3.cs
@@ -0,0 +1,31 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using System.IO;
+
+namespace DotNetToolBox.Cryptography
+{
+    public static class SHA3
+    {
+        /// <summary>
+        /// Compute the MD5 hash value
+        /// </summary>
+        /// <param name="input">Input Stream</param>
+        public static byte[] Hash(Stream input, int bitLength = 512)
+        {
+            Sha3Digest sha3 = new Sha3Digest(bitLength);
+            int bytesRead;
+            byte[] buffer = new byte[4096];
+
+            do
+            {
+                bytesRead = input.Read(buffer, 0, 4096);
+                if (bytesRead > 0)
+                    sha3.BlockUpdate(buffer, 0, bytesRead);
+            } while (bytesRead == 4096);
+
+            byte[] result = new byte[bitLength / 8];
+            sha3.DoFinal(result, 0);
+
+            return result;
+        }
+    }
+}
