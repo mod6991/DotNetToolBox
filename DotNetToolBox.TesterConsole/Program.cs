c03f26bd85b1439bba4a7791d1d4f01915b744e4
Josue Clement <mod6991@gmail.com>
2021-04-03 09:34:21 +0200

    Save changes

--- DotNetToolBox.TesterConsole/Program.cs
+++ DotNetToolBox.TesterConsole/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DotNetToolBox.Cryptography;
+using DotNetToolBox.IO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,14 @@ namespace DotNetToolBox.TesterConsole
     {
         static void Main(string[] args)
         {
+            byte[] data = Encoding.ASCII.GetBytes("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+            byte[] result;
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                result = SHA3.Hash(ms);
+            }
 
+            string s = Hex.Encode(result);
         }
     }
 }
