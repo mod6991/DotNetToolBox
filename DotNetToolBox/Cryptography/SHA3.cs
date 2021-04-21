9ec4eba6333c9b68849dce1c014533d9f5f99558
Josue Clement <mod6991@gmail.com>
2021-04-21 22:49:07 +0200

    Change Hash method

--- DotNetToolBox/Cryptography/SHA3.cs
+++ DotNetToolBox/Cryptography/SHA3.cs
@@ -26,26 +26,13 @@ namespace DotNetToolBox.Cryptography
 {
     public static class SHA3
     {
-        /// <summary>
-        /// Compute the MD5 hash value
-        /// </summary>
-        /// <param name="input">Input Stream</param>
-        public static byte[] Hash(Stream input, int bitLength = 512)
+
+        public static byte[] Hash(byte[] data, int bitLength = 512)
         {
             Sha3Digest sha3 = new Sha3Digest(bitLength);
-            int bytesRead;
-            byte[] buffer = new byte[4096];
-
-            do
-            {
-                bytesRead = input.Read(buffer, 0, 4096);
-                if (bytesRead > 0)
-                    sha3.BlockUpdate(buffer, 0, bytesRead);
-            } while (bytesRead == 4096);
-
+            sha3.BlockUpdate(data, 0, data.Length);
             byte[] result = new byte[bitLength / 8];
             sha3.DoFinal(result, 0);
-
             return result;
         }
     }
