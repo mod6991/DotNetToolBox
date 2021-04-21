e9a426cea653fbc569bb04adf5e4e1115b07d2c7
Josue Clement <mod6991@gmail.com>
2021-04-21 22:40:57 +0200

    Add tests

---
+++ DotNetToolBox.UnitTests/TripleDESTests.cs
@@ -0,0 +1,58 @@
+﻿using DotNetToolBox.Cryptography;
+using DotNetToolBox.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNetToolBox.UnitTests
+{
+    [TestClass]
+    public class TripleDESTests
+    {
+        private TestContext _testContext;
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value; }
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\des3.csv", "des3#csv", DataAccessMethod.Sequential)]
+        public void Encrypt_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sIv = TestContext.DataRow["iv"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] iv = Hex.Decode(sIv);
+            byte[] data = Hex.Decode(sData);
+
+            byte[] enc = TripleDES.Encrypt(data, key, iv);
+            string result = Hex.Encode(enc);
+
+            Assert.AreEqual(sEnc, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\des3.csv", "des3#csv", DataAccessMethod.Sequential)]
+        public void Decrypt_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sIv = TestContext.DataRow["iv"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] iv = Hex.Decode(sIv);
+            byte[] enc = Hex.Decode(sEnc);
+
+            byte[] dec = TripleDES.Decrypt(enc, key, iv);
+            string result = Hex.Encode(dec);
+
+            Assert.AreEqual(sData, result);
+        }
+    }
+}
