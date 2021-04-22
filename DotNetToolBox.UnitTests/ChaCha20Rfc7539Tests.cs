c81decbb8c2090bcddf7e7d662bfe6ea1c6a637b
Josue Clement <mod6991@gmail.com>
2021-04-22 16:45:35 +0200

    Add stream tests

--- DotNetToolBox.UnitTests/ChaCha20Rfc7539Tests.cs
+++ DotNetToolBox.UnitTests/ChaCha20Rfc7539Tests.cs
@@ -1,7 +1,7 @@
 ﻿using DotNetToolBox.Cryptography;
 using DotNetToolBox.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.IO;
 
 namespace DotNetToolBox.UnitTests
 {
@@ -38,6 +38,35 @@ namespace DotNetToolBox.UnitTests
         [TestMethod]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
             @"|DataDirectory|\data\chacha20rfc7539.csv", "chacha20rfc7539#csv", DataAccessMethod.Sequential)]
+        public void Encrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sNonce = TestContext.DataRow["nonce"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] nonce = Hex.Decode(sNonce);
+            byte[] data = Hex.Decode(sData);
+
+            byte[] enc;
+            using(MemoryStream msIn = new MemoryStream(data))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    ChaCha20Rfc7539.Encrypt(msIn, msOut, key, nonce);
+                    enc = msOut.ToArray();
+                }
+            }
+
+            string result = Hex.Encode(enc);
+
+            Assert.AreEqual(sEnc, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\chacha20rfc7539.csv", "chacha20rfc7539#csv", DataAccessMethod.Sequential)]
         public void Decrypt_Tests()
         {
             string sKey = TestContext.DataRow["key"].ToString();
@@ -50,6 +79,35 @@ namespace DotNetToolBox.UnitTests
             byte[] enc = Hex.Decode(sEnc);
 
             byte[] dec = ChaCha20Rfc7539.Decrypt(enc, key, nonce);
+            string result = Hex.Encode(dec);
+
+            Assert.AreEqual(sData, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\chacha20rfc7539.csv", "chacha20rfc7539#csv", DataAccessMethod.Sequential)]
+        public void Decrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sNonce = TestContext.DataRow["nonce"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] nonce = Hex.Decode(sNonce);
+            byte[] enc = Hex.Decode(sEnc);
+
+            byte[] dec;
+            using(MemoryStream msIn = new MemoryStream(enc))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    ChaCha20Rfc7539.Decrypt(msIn, msOut, key, nonce);
+                    dec = msOut.ToArray();
+                }
+            }
+
             string result = Hex.Encode(dec);
 
             Assert.AreEqual(sData, result);
--- DotNetToolBox.UnitTests/ChaCha20Tests.cs
+++ DotNetToolBox.UnitTests/ChaCha20Tests.cs
@@ -2,6 +2,7 @@
 using DotNetToolBox.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace DotNetToolBox.UnitTests
 {
@@ -38,6 +39,35 @@ namespace DotNetToolBox.UnitTests
         [TestMethod]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
             @"|DataDirectory|\data\chacha20.csv", "chacha20#csv", DataAccessMethod.Sequential)]
+        public void Encrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sNonce = TestContext.DataRow["nonce"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] nonce = Hex.Decode(sNonce);
+            byte[] data = Hex.Decode(sData);
+
+            byte[] enc;
+            using(MemoryStream msIn = new MemoryStream(data))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    ChaCha20.Encrypt(msIn, msOut, key, nonce);
+                    enc = msOut.ToArray();
+                }
+            }
+
+            string result = Hex.Encode(enc);
+
+            Assert.AreEqual(sEnc, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\chacha20.csv", "chacha20#csv", DataAccessMethod.Sequential)]
         public void Decrypt_Tests()
         {
             string sKey = TestContext.DataRow["key"].ToString();
@@ -50,6 +80,35 @@ namespace DotNetToolBox.UnitTests
             byte[] enc = Hex.Decode(sEnc);
 
             byte[] dec = ChaCha20.Decrypt(enc, key, nonce);
+            string result = Hex.Encode(dec);
+
+            Assert.AreEqual(sData, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\chacha20.csv", "chacha20#csv", DataAccessMethod.Sequential)]
+        public void Decrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sNonce = TestContext.DataRow["nonce"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] nonce = Hex.Decode(sNonce);
+            byte[] enc = Hex.Decode(sEnc);
+
+            byte[] dec;
+            using(MemoryStream msIn = new MemoryStream(enc))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    ChaCha20.Decrypt(msIn, msOut, key, nonce);
+                    dec = msOut.ToArray();
+                }
+            }
+
             string result = Hex.Encode(dec);
 
             Assert.AreEqual(sData, result);
