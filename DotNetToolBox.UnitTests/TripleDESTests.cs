f67c04d7a14bfe676c73aac019c5041bbdc3a905
Josue Clement <mod6991@gmail.com>
2021-04-22 17:03:06 +0200

    Add stream tests

--- DotNetToolBox.UnitTests/TripleDESTests.cs
+++ DotNetToolBox.UnitTests/TripleDESTests.cs
@@ -2,6 +2,7 @@
 using DotNetToolBox.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace DotNetToolBox.UnitTests
 {
@@ -38,6 +39,35 @@ namespace DotNetToolBox.UnitTests
         [TestMethod]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
             @"|DataDirectory|\data\des3.csv", "des3#csv", DataAccessMethod.Sequential)]
+        public void Encrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sIv = TestContext.DataRow["iv"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] iv = Hex.Decode(sIv);
+            byte[] data = Hex.Decode(sData);
+
+            byte[] enc;
+            using(MemoryStream msIn = new MemoryStream(data))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    TripleDES.Encrypt(msIn, msOut, key, iv, PaddingStyle.None);
+                    enc = msOut.ToArray();
+                }
+            }
+
+            string result = Hex.Encode(enc);
+
+            Assert.AreEqual(sEnc, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\des3.csv", "des3#csv", DataAccessMethod.Sequential)]
         public void Decrypt_Tests()
         {
             string sKey = TestContext.DataRow["key"].ToString();
@@ -50,6 +80,35 @@ namespace DotNetToolBox.UnitTests
             byte[] enc = Hex.Decode(sEnc);
 
             byte[] dec = TripleDES.Decrypt(enc, key, iv);
+            string result = Hex.Encode(dec);
+
+            Assert.AreEqual(sData, result);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"|DataDirectory|\data\des3.csv", "des3#csv", DataAccessMethod.Sequential)]
+        public void Decrypt_Stream_Tests()
+        {
+            string sKey = TestContext.DataRow["key"].ToString();
+            string sIv = TestContext.DataRow["iv"].ToString();
+            string sData = TestContext.DataRow["data"].ToString();
+            string sEnc = TestContext.DataRow["enc"].ToString();
+
+            byte[] key = Hex.Decode(sKey);
+            byte[] iv = Hex.Decode(sIv);
+            byte[] enc = Hex.Decode(sEnc);
+
+            byte[] dec;
+            using(MemoryStream msIn = new MemoryStream(enc))
+            {
+                using (MemoryStream msOut = new MemoryStream())
+                {
+                    TripleDES.Decrypt(msIn, msOut, key, iv, PaddingStyle.None);
+                    dec = msOut.ToArray();
+                }
+            }
+
             string result = Hex.Encode(dec);
 
             Assert.AreEqual(sData, result);
