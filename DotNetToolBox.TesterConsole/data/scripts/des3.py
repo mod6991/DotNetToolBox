bb1467238657c6c4224083566f933bfcee65d9cf
Josue Clement <mod6991@gmail.com>
2021-05-02 22:09:07 +0200

    Add data + scripts

---
+++ DotNetToolBox.TesterConsole/data/scripts/des3.py
@@ -0,0 +1,43 @@
+from struct import pack, unpack
+from typing import BinaryIO
+from io import BytesIO
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import DES3
+
+
+def read_lv(stream: BinaryIO):
+    data_len = stream.read(4)
+    value_len = unpack('i', data_len)[0]
+    return stream.read(value_len)
+
+
+def write_lv(stream: BinaryIO, value: bytes):
+    data_len = pack('i', len(value))
+    stream.write(data_len)
+    stream.write(value)
+
+
+def main():
+    with open(r'C:\Temp\testData\des3.dat', 'wb') as file:
+        for i in range(1, 300 + 1):
+            with BytesIO() as ms:
+                key = get_random_bytes(24)
+                iv = get_random_bytes(8)
+                data = get_random_bytes(i * 8)
+                write_lv(ms, key)
+                write_lv(ms, iv)
+                write_lv(ms, data)
+
+                cipher = DES3.new(key, DES3.MODE_CBC, iv)
+                enc = cipher.encrypt(data)
+                write_lv(ms, enc)
+
+                ms.seek(0)
+                record = ms.read()
+
+            write_lv(file, record)
+        write_lv(file, b'')
+
+
+if __name__ == '__main__':
+    main()
